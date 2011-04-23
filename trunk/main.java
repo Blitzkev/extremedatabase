fd32f672d4e09ac6f57f90eef0868b3f37f9f7e8
thatguydrew46@gmail.com <thatguydrew46@gmail.com@77f75bf6-3c5f-3a56-6a8c-94d130f48962>
2011-04-23 17:34:09 +0000

    Added comments and instructions to log into Oracle via the command-line and sqlplus

--- trunk/main.java
+++ trunk/main.java
@@ -1,13 +1,25 @@
 /* Account info
- *Unix:
+ * 
+ * We get a dedicated CS Account:
+ * 
  * Login: p485-70a
- * Pwd: Please.Change.This
-
- *Oracle:
- * Login: p48570a
- * Pwd: pidooyiengei
+ * Pwd: Extreme.Database
+ *
+ * This is setting up the variables to use Oracle via command-line.
+ * 
+ * source /usr/local/bin/coraenv
+ * <Return Key>
+ * 
+ * Put this into the .cshrc file: source /usr/local/bin/coraenv
+ * 
+ * This is for Oracle access via sqlplus in the command-line (queeg):
+ * 
+ * sqlplus p48570a@csodb10
+ * 
+ * When prompted for password, enter this:
+ * 
+ * Enter Password: letmein
  */
-
 
 public class main {
 
