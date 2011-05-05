42a564365702de2e5b892bb886ad527c2bad6bee
thatguydrew46@gmail.com <thatguydrew46@gmail.com@77f75bf6-3c5f-3a56-6a8c-94d130f48962>
2011-05-05 23:43:07 +0000

    Added a useful note to main which allows us to view all tables stored in the database

---
+++ trunk/ExtremeDatabase.java
@@ -0,0 +1,14 @@
+/**
+ * Establishes a connection to the Oracle database system.
+ * Also holds the main method.
+ * 
+ * @author Andrew Sinn
+ *
+ */
+
+public class ExtremeDatabase {
+
+	public static void main(String[]args) {
+		
+	}
+}
